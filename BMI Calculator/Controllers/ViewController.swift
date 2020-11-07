288435f4da0bf38c5a93a52e615f37ed70bd5931
Alex Sukhitashvili <sukhitashvilialex2@gmail.com>
2020-11-07 19:49:14 +0400

    feat: bind slider to labels

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var height: UILabel!
+    
+    @IBOutlet weak var weight: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+        height.text = String(format: "%.2fm", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weight.text = String(format: "%.0fKg", sender.value)
+        
+    }
+    
 }
 
